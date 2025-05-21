d2311236ff3dfbd59e7e67758ac99cd23ef9fa44
Gustavo Ávila <71139006+gusavila@users.noreply.github.com>
2025-05-21 17:47:37 -0300

    Criado variavel de ambiente

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import cors from "cors";
 import pg from "pg";
+import env from "dotenv";
+
+env.config();
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "todolist",
-  password: "Costa@1vip",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 
 db.connect();

a67ff3fab77b8b8d2d031921af788779f28787cc
Gustavo Ávila <71139006+gusavila@users.noreply.github.com>
2025-05-24 19:06:56 -0300

    Create footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="absolute inset-x-0 bottom-0 text-center h-8">
+      <p>Criado com ❤️ por Gustavo Ávila</p>
+    </div>
+  );
+};
+
+export default Footer;
